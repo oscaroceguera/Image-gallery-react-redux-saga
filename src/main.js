0820f600874da0e0594737d52c51f9601cfbe8fd
oscaroceguerabibriesca <oscaroceguera@gmail.com>
2016-12-09 00:08:47 -0700

    Updating the state

--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@ import 'babel-polyfill'
 
 import React from 'react'
 import ReactDom from 'react-dom'
+import Gallery from './Gallery'
+
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+import { Provider } from 'react-redux'
 
 ReactDom.render(
-  <h1>Holas</h1>,
+  <Provider store={store}>
+    <Gallery />
+  </Provider>,
   document.getElementById('root')
 )
