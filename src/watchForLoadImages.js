7daf0f4105928412fec00c9334c8c461a9d06f53
oscaroceguerabibriesca <oscaroceguera@gmail.com>
2016-12-09 00:38:14 -0700

    Handling errors

--- src/watchForLoadImages.js
+++ src/watchForLoadImages.js
@@ -2,9 +2,13 @@ import {fetchImages} from './flickr';
 import {put, take, fork} from 'redux-saga/effects';
 
 export function* loadImages() {
-  const images = yield fetchImages();
-  yield put({type: 'IMAGES_LOADED', images})
-  yield put({type: 'IMAGE_SELECTED', image: images[2]})
+  try {
+    const images = yield fetchImages();
+    yield put({type: 'IMAGES_LOADED', images})
+    yield put({type: 'IMAGE_SELECTED', image: images[2]})
+  } catch (err) {
+    yield put({type: 'IMAGE_LOAD_FAILURE', error})
+  }
 }
 
 export function* watchForLoadImages() {
